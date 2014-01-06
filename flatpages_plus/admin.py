b657ad25f4802bc472a49132b65a30c31e8bcdae
Oleg Dolya <oleg.dolya@gmail.com>
2014-01-06 17:32:50 +0200

    modify support model translate

--- flatpages_plus/admin.py
+++ flatpages_plus/admin.py
@@ -2,11 +2,17 @@
 from django.conf import settings
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from flatpages_plus.forms import FlatpageForm
+# from flatpages_plus.forms import FlatpageForm
 from flatpages_plus.models import FlatPage, Categories
-from modeltranslation.admin import TranslationAdmin
-from fiber.models import ContentItem
+#from fiber.models import ContentItem
+try:
+    from modeltranslation.admin import TranslationAdmin, TranslationStackedInline
 
+    ModelAdmin = TranslationAdmin
+    StackedInlineAdmin = TranslationStackedInline
+except:
+    ModelAdmin = admin.ModelAdmin
+    StackedInlineAdmin = admin.StackedInline
 
 #try:
 #    from grappellifit.admin import TranslationAdmin, TranslationStackedInline
@@ -21,11 +27,8 @@ from fiber.models import ContentItem
 #        ModelAdmin = admin.ModelAdmin
 #        StackedInlineAdmin = admin.StackedInline
 
-ModelAdmin = admin.ModelAdmin
-StackedInlineAdmin = admin.StackedInline
 
-
-class CategoriesAdmin(TranslationAdmin):
+class CategoriesAdmin(ModelAdmin):
     list_display = ('name', 'is_enable', 'slug', 'status',)
     list_filter = ('is_enable', 'status',)
     list_editable = ('slug', 'status',)
@@ -36,17 +39,17 @@ class CategoriesAdmin(TranslationAdmin):
         js = [
             "{0}grappelli_modeltranslation/js/tabbed_translation_fields.js".format(settings.STATIC_URL),
         ]
-        css = {'screen': '{0}grappelli_modeltranslation/css/css/tabbed_translation_fields.css'.format(settings.STATIC_URL)}
+        css = {
+            'screen': '{0}grappelli_modeltranslation/css/css/tabbed_translation_fields.css'.format(settings.STATIC_URL)}
 
 
-class FlatPageAdmin(TranslationAdmin):
-    #form = FlatpageForm
+class FlatPageAdmin(ModelAdmin):
     fieldsets = (
         (_('Title'), {
             'fields': ('title',)
         }),
         (_('Title'), {
-            'fields': (('url', 'gonews', 'category',), ('date_publish', 'date_unpublish',),)
+            'fields': (('url', 'gonews', 'category'), ('date_publish', 'date_unpublish'))
         }),
         (_('Description'), {
             'classes': ('grp-collapse grp-closed',),
@@ -57,13 +60,13 @@ class FlatPageAdmin(TranslationAdmin):
         )}),
         (_(u'Основной текст'), {'fields': (
             'content',
-            )}),
+        )}),
         (_('Advanced options'), {
             'classes': ('grp-collapse grp-closed',),
             'fields': (
                 ('owner', 'status',),
-                 'tags',
-                 'sites',
+                'tags',
+                'sites',
                 ('enable_comments', 'enable_social',),
                 'registration_required',
                 'template_name',
@@ -72,9 +75,8 @@ class FlatPageAdmin(TranslationAdmin):
         }),
         (_('SEO'), {
             'classes': ('grp-collapse grp-closed',),
-            'fields': ('meta_keywords', 'meta_description',),
-            }),
-        )
+            'fields': ('meta_keywords', 'meta_description',)}),
+    )
     #prepopulated_fields = {'url': ('title',)}
     list_display = ('url', 'title', 'date_publish', 'category', 'order', 'status', 'owner', 'views', 'modified',)
     list_filter = ('status', 'sites', 'enable_comments', 'registration_required', 'enable_social', 'category',)
@@ -83,14 +85,15 @@ class FlatPageAdmin(TranslationAdmin):
     search_fields = ('url', 'title', 'name', 'owner',)
     date_hierarchy = 'date_publish'
     #inlines = [NewsPhotoInline, NewsVideoInline, NewsDocumentInline]
+
     class Media:
-            js = [
-                "{0}grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js".format(settings.STATIC_URL),
-                "{0}js/tinymce_setup.js".format(settings.STATIC_URL),
-            ]
+        js = [
+            "{0}grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js".format(settings.STATIC_URL),
+            "{0}js/tinymce_setup.js".format(settings.STATIC_URL),
+        ]
 
 
-class FiberContentItemAdmin(TranslationAdmin):
+class FiberContentItemAdmin(ModelAdmin):
     group_fieldsets = True
     list_display = ('__unicode__',)
     fieldsets = (
@@ -104,7 +107,8 @@ class FiberContentItemAdmin(TranslationAdmin):
             "{0}grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js".format(settings.STATIC_URL),
             "{0}js/tinymce_setup.js".format(settings.STATIC_URL),
         ]
-        css = {'screen': '{0}grappelli_modeltranslation/css/css/tabbed_translation_fields.css'.format(settings.STATIC_URL)}
+        css = {
+            'screen': '{0}grappelli_modeltranslation/css/css/tabbed_translation_fields.css'.format(settings.STATIC_URL)}
 
 
 admin.site.register(FlatPage, FlatPageAdmin)
